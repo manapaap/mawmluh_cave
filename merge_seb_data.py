a6391ebfb12a41f56b226ce8dc57365e8ef00cf3
Aakash Manapat <aakash.p.manapat@vanderbilt.edu>
2022-04-28 18:26:22 -0500

    my commit

---
+++ merge_seb_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 18:10:31 2022
+
+@author: Aakas
+"""
+
+from os import chdir
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+chdir('C:/Users/Aakas/Documents/School/Oster_lab/programs')
